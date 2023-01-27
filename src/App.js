06fa917d5a39cc7224a4b1e5fe90acb6c732f65e
hankyeol-jung <whd6023@gmail.com>
2023-01-27 11:21:22 +0900

    헤더메뉴 실시간 시계반영

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import logo from "./images/logo.png";
 import userIcon from "./images/user.png";
 import arrowBottom from "./images/arrow-bottom.png";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSeedling, faWater } from "@fortawesome/free-solid-svg-icons";
+
 import moment from "moment";
 // 안써도 자동으로 한국 시간을 불러온다. 명확하게 하기 위해 import
 import "moment/locale/ko";
 
-// import { useInterval } from "react-use";
+import Clock from "react-live-clock";
 
 import { useState, useRef, useEffect, useBoolean } from "react";
 
 function App() {
-  const nowTime = moment().format("hh:mm:ss");
-
-  let newTime = setInterval(nowTime, 1000);
-
-  console.log(newTime);
+  let timeFirst = moment().format("a");
 
   return (
     <div className="App bg-slate-200 w-screen h-screen">
@@ -26,8 +25,12 @@ function App() {
           <img src={logo} className="w-[12.25rem]"></img>
         </div>
         <div className="w-1/3 flex justify-center items-end">
-          <p className="text-2xl text-neutral-600 font-medium mr-3"></p>
-          <p className="text-4xl text-neutral-700 font-bold">{nowTime}</p>
+          <p className="text-2xl text-neutral-600 font-medium mr-3">
+            {timeFirst == "am" ? "오전" : "오후"}
+          </p>
+          <p className="text-4xl text-neutral-700 font-bold">
+            <Clock format="hh:mm" ticking={true}></Clock>
+          </p>
         </div>
         <div className="w-1/3 flex justify-end items-center">
           <p className="text-xl text-neutral-700 font-bold mr-3">홍길동님</p>
@@ -36,6 +39,16 @@ function App() {
             <img src={arrowBottom} className="w-6"></img>
           </span>
         </div>
+      </div>
+
+      {/* 네비게이션 메뉴 */}
+      <div className="bg-white grid grid-cols-8 h-[108px]">
+        <span className="flex flex-col justify-center items-center cursor-pointer relative before:w-[2px] before:h-12 before:bg-neutral-300 before:absolute before:right-0 before:top-1/2 before:-translate-y-1/2 before:translate-x-1/2 last-of-type:before:hidden">
+          <div className="text-2xl text-gray-600 mb-2 w-10 h-10 flex justify-center items-center">
+            <FontAwesomeIcon icon={faSeedling} />
+          </div>
+          <p className="text-xl font-medium text-gray-600">환경</p>
+        </span>
       </div>
     </div>
   );
