146661f89a77e159e1c38dab0bd44bcf0c0fef08
hankyeol-jung <whd6023@gmail.com>
2023-02-06 15:28:53 +0900

    test

--- src/page/Weather.js
+++ src/page/Weather.js
@@ -1,52 +1,52 @@
 import { useState } from "react";
 
-const api = {
-  key: "146cea1eef706828403d6859379129b5",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+// const api = {
+//   key: "146cea1eef706828403d6859379129b5",
+//   base: "https://api.openweathermap.org/data/2.5/",
+// };
 
-const dateBuilder = (d) => {
-  let months = [
-    "january",
-    "February",
-    "March",
-    "April",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+// const dateBuilder = (d) => {
+//   let months = [
+//     "january",
+//     "February",
+//     "March",
+//     "April",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+//   ];
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
+//   let day = days[d.getDay()];
+//   let date = d.getDate();
+//   let month = months[d.getMonth()];
+//   let year = d.getFullYear();
 
-  return "dsfiojweo" + `${day} ${date} ${month} ${year}`;
-};
+//   return "dsfiojweo" + `${day} ${date} ${month} ${year}`;
+// };
 
 function Weather() {
-  function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
-  }
-  function onGeoError() {
-    alert("Can't find you. No weather for you.");
-  }
-  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+  // function onGeoOk(position) {
+  //   const lat = position.coords.latitude;
+  //   const lng = position.coords.longitude;
+  //   console.log("You live in", lat, lng);
+  // }
+  // function onGeoError() {
+  //   alert("Can't find you. No weather for you.");
+  // }
+  // navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
   return <div>ddd</div>;
 }
